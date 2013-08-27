0667a5b7a3d4b3480ca7bdb05c7e63fec76dbe88
Paulo <paulogabriel13@gmail.com>
2013-08-27 11:48:31 -0300

    Corrigindo BUG Sessao.Java

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -5,13 +5,12 @@ import org.junit.Test;
 
 public class SessaoTest {
 
-
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
 		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
+		sessao.setTotalIngressos(2);
 
-        Assert.assertTrue(sessao.podeReservar(1));
+		Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
@@ -23,11 +22,23 @@ public class SessaoTest {
 	}
 
 	@Test
-	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
+	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis()
+			throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
+
+	@Test
+	public void deveSerPossivelreservarTodosOsIngressosDisponiveis()
+			throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(45000);
+
+		Assert.assertTrue(sessao.podeReservar(45000));
+
+	}
+
 }
